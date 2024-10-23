87b47710aa56e2ffc8fe389f19032c68f2ca3bda
aamerfaiz <aamrfz@gmail.com>
2024-10-24 02:33:24 +0530

    remove unnecessary nav

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="text-white flex flex-row justify-between items-center mx-[2vw]">
-      <Link href="/">Aamer Faiz</Link>
+      <Link href="/">Portfolio</Link>
       <div className="h-16 flex flex-row justify-end text-white items-center">
         <a href="/documents/resume.pdf" download className="mx-[1vw] cursor-pointer">
           My Resume
